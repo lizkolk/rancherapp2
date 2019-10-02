4d7fc230577c8c84bf6455b18ae169f4dc3babcc
Liz Kolk <liz.kolk@zirmed.com>
2019-10-02 14:18:02 -0400

    Add project files.

---
+++ rancherapp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+
+namespace rancherapp
+{
+    public class Program
+    {
+            public static void Main(string[] args)
+            {
+                BuildWebHost(args).Run();
+            }
+            public static IWebHost BuildWebHost(string[] args) =>
+                WebHost.CreateDefaultBuilder(args)
+                    .UseStartup<Startup>()
+                    .Build();
+
+        }
+    }
+    //    var host = new WebHostBuilder()
+    //        .UseKestrel()
+    //        .UseContentRoot(Directory.GetCurrentDirectory())
+    //        .UseIISIntegration()
+    //        .UseStartup<Startup>()
+    //        .UseApplicationInsights()
+    //        .UseUrls("http://0.0.0.0:8080")
+    //        .Build();
+    //    host.Run();
+    //}
+
